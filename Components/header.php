5f874f950fbc4df6c795346bc3bdf0f33ae4f23d
Joshanjohn <54502617+joshanjohn@users.noreply.github.com>
2024-04-22 20:53:13 +0100

    fixed navigation in nav bar

--- Components/header.php
+++ Components/header.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+$_SESSION['access'] = 'public';
 
 // Including the database connection
 if ($_SERVER['SERVER_NAME'] == 'knuth.griffith.ie') {
@@ -11,33 +12,25 @@ if ($_SERVER['SERVER_NAME'] == 'knuth.griffith.ie') {
 }
 require $path_to_mysql_connect;
 
+function getbaseURL()
+{
+    // Get the full URL from the browser
+    $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http";
+    $url = $protocol . "://" . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+
+    // Find the position of "WanderWaves" in the URL
+    $position = strpos($url, 'WanderWaves');
+
+    // Return the URL up to "WanderWaves"
+    if ($position !== false) {
+        return substr($url, 0, $position + strlen('WanderWaves'));
+    }
+
+    return $url; // Return the original URL if "WanderWaves" is not found
+}
+
 
 $userLevel = $_SESSION['access'];
-
-$navLinks = array(
-    'base' => array(
-        './index.php',
-        '/pages/propertyListings/propertyListings.php',
-        '/pages/inventoryDetails',
-        '/pages/tenantAccount',
-        '/pages/landlordAccount',
-        '#testimonial',
-        '#contact',
-        './pages/logIn/Login.php',
-        './pages/logOut/'
-    ),
-    'pages' => array(
-        '../../index.php',
-        '../pages/propertyListings/propertyListings.php',
-        '../pages/inventoryDetails',
-        '../pages/tenantAccount',
-        '../pages/landlordAccount',
-        '../../index.php#testimonial',
-        '../../index.php#contact',
-        '../pages/logIn/Login.php',
-        '../pages/logOut/'
-    ),
-);
 
 ?>
 
@@ -60,112 +53,72 @@ $navLinks = array(
         <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
             <ul class="navbar-nav mb-2 mb-lg-0">
                 <?php
+                echo '<li class="nav-item">';
+                echo '<a class="nav-link" href="' . getbaseURL() . '/index.php">Home</a>';
+                echo '</li>';
 
-                if (findDir() == 'base') {
-                    echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['base'][0] . '">Home</a>';
-                    echo '</li>';
+                echo '<li class="nav-item">';
+                echo '<a class="nav-link" href="' . getbaseURL() . '/index.php#testimonial">Testimonials</a>';
+                echo '</li>';
 
+                if ($userLevel == 'tenants' || $userLevel == 'admin' || $userLevel == 'landlord') {
+                    if ($userLevel == 'landlord' || $userLevel == 'admin') {
+                        if ($userLevel == 'admin') {
 
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['base'][1] . '">Property Listings</a>';
-                        echo '</li>';
-
+                        }
                     }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['base'][2] . '">Inventory Details</a>';
-                        echo '</li>';
-
-                    }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['base'][3] . '">Tenant Account</a>';
-                        echo '</li>';
-
-                    }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['base'][4] . '">Landlord Account</a>';
-                        echo '</li>';
-
-                    }
-                    echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['base'][5] . '">Testimonials</a>';
-                    echo '</li>';
 
                     echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['base'][6] . '">Contact Us</a>';
+                    echo '<a class="nav-link" href="' . getbaseURL() . '/pages/logIn/LogIn.php">Log Out</a>';
                     echo '</li>';
-
-                    echo '<li class="nav-item">';
-                    if ($userLevel == "Public") {
-                        echo '<a class="nav-link" href="' . $navLinks['base'][7] . '">Log in</a>';
-                    } else {
-                        echo '<a class="nav-link" href="' . $navLinks['base'][8] . '">Log Out</a>';
-                    }
-                    echo '</li>';
-
-
                 } else {
                     echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['pages'][0] . '">Home</a>';
+                    echo '<a class="nav-link" href="' . getbaseURL() . '/pages/logIn/LogIn.php">Login</a>';
                     echo '</li>';
-
-
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][1] . '">Property Listings</a>';
-                        echo '</li>';
-
-                    }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][2] . '">Inventory Details</a>';
-                        echo '</li>';
-
-                    }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][3] . '">Tenant Account</a>';
-                        echo '</li>';
-
-                    }
-                    if ($userLevel != "Public") {
-                        echo '<li class="nav-item">';
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][4] . '">Landlord Account</a>';
-                        echo '</li>';
-
-                    }
-                    echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['pages'][5] . '">Testimonials</a>';
-                    echo '</li>';
-
-                    echo '<li class="nav-item">';
-                    echo '<a class="nav-link" href="' . $navLinks['pages'][6] . '">Contact Us</a>';
-                    echo '</li>';
-
-                    echo '<li class="nav-item">';
-                    if ($userLevel == "Public") {
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][7] . '">Log in</a>';
-                    } else {
-                        echo '<a class="nav-link" href="' . $navLinks['pages'][8] . '">Log Out</a>';
-                    }
-                    echo '</li>';
-
                 }
 
 
+                // if ($userLevel == "admin" ) {
+                //     echo '<li class="nav-item">';
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][1] . '">Property Listings</a>';
+                //     echo '</li>';
+                
+                // }
+                // if ($userLevel != "Public") {
+                //     echo '<li class="nav-item">';
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][2] . '">Inventory Details</a>';
+                //     echo '</li>';
+                
+                // }
+                // if ($userLevel != "Public") {
+                //     echo '<li class="nav-item">';
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][3] . '">Tenant Account</a>';
+                //     echo '</li>';
+                
+                // }
+                // if ($userLevel != "Public") {
+                //     echo '<li class="nav-item">';
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][4] . '">Landlord Account</a>';
+                //     echo '</li>';
+                
+                // }
+                // echo '<li class="nav-item">';
+                // echo '<a class="nav-link" href="' . $navLinks['base'][5] . '">Testimonials</a>';
+                // echo '</li>';
+                
+                // echo '<li class="nav-item">';
+                // echo '<a class="nav-link" href="' . $navLinks['base'][6] . '">Contact Us</a>';
+                // echo '</li>';
+                
+                // echo '<li class="nav-item">';
+                // if ($userLevel == "Public") {
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][7] . '">Log in</a>';
+                // } else {
+                //     echo '<a class="nav-link" href="' . $navLinks['base'][8] . '">Log Out</a>';
+                // }
+                // echo '</li>';
+                
 
-                function findDir()
-                {
-                    if (str_contains($_SERVER['PHP_SELF'], "pages")) {
-                        return 'pages';
-                    } else {
-                        return 'base';
-                    }
-                }
                 ?>
 
 
--- index.php
+++ index.php
@@ -38,10 +38,6 @@
         </div>
     </section>
 
-
-
-
-
     <?php
     //ADVERTS SECTION
     include 'pages/adverts/adverts.php';
@@ -58,8 +54,12 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js"
+        integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"
+        integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>

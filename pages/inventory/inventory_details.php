b40dfc06772efbc548859295d3d87adb123d0f0a
anna <2004gromyko@gmail.com>
2024-04-23 02:12:13 +0100

    Implemented Inventory_details page and added design

--- pages/inventory/inventory_details.php
+++ pages/inventory/inventory_details.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home Page</title>
+    <title>Inventory page</title>
     <link rel="stylesheet" href="../../Assets/css/index.css?v=<?php echo time(); ?>">
     <!-- <link href="Assets/css/index.css" rel="stylesheet"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -20,19 +20,71 @@
 
     <?php
     include '../../Components/header.php';
+
+    function isIncluded($data)
+    {
+        $stmnt = '';
+        if ($data == 1) {
+            $stmnt = "Yes";
+        } else {
+            $stmnt = "No";
+        }
+        return $stmnt;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        if (!empty($_POST['eircode'])) {
+            $eircode = $_POST['eircode'];
+        } else {
+            echo "Property Eircode is required";
+        }
+        $sql = "SELECT a.* 
+        FROM appliances a
+        JOIN property p ON a.property_id = p.property_id
+        WHERE p.eircode = '$eircode'";
+
+        $result = mysqli_query($db_connection, $sql);
+        if (mysqli_num_rows($result) == 0) {
+            echo "<div class='Message'><p>No properties found matching the criteria.</p></div>";
+        } else {
+            echo "<div class='container' id = 'appliance_container'>";
+            echo "<h2 class='mt-5 text-center'>Property Inventory Details</h2>";
+
+            while ($row = mysqli_fetch_assoc($result)) {
+                echo "<div class='card mt-3' id = 'appliance_details'>";
+                echo "<div class='card-body'>";
+                echo "<p class='card-title'>Property ID: " . $row["property_id"] . "</p>";
+                echo "<p class='card-text'>Washing machine: " . isIncluded($row["washing_machine"]) . "</p>";
+                echo "<p class='card-text'>Microwave: " . isIncluded($row["microwave"]) . "</p>";
+                echo "<p class='card-text'>Dryer: " . isIncluded($row["dryer"]) . "</p>";
+                echo "<p class='card-text'>Wi-Fi: " . isIncluded($row["wifi"]) . "</p>";
+                echo "<p class='card-text'TV: " . isIncluded($row["tv"]) . "</p>";
+                echo "</div>";
+                echo "</div>";
+            }
+
+
+
+            echo "</div>";
+        }
+    }
     ?>
-    <?php
-    //ADVERTS SECTION
-    include '..//adverts/adverts.php';
+    <div id="invent_content">
 
-    // TESTIMONIAL SECTION
-    include '..//testimonials/testimonial.php';
+        <div class="col-md-9 col-lg-6 col-xl-5">
+            <img src="../../Assets/images/index2.png" class="img-fluid" alt="Sample image">
+        </div>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="POST" novalidate class="mt-3"
+            id="inventory">
+            <div class="form-group">
+                <h2><label for="eircode">Eircode</label></h2>
+                <input type="text" class="form-control" id="eircode" name="eircode">
+            </div><br>
+            <button type="submit" class="btn btn-primary">Find</button>
 
-    include '../testimonials/testimonial_add.php';
+        </form>
 
-    // FOOTER
-    include '../contact/contactUs.php';
-    ?>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"

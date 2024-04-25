9c9e52b7267f99cf687ef6aaafef230d780c9273
Getrude <getrude.langat@student.griffith.ie>
2024-04-25 01:09:51 +0100

    added property, property_edit and feature_boxes

--- index.php
+++ index.php
@@ -8,7 +8,11 @@
     <link rel="stylesheet" href="Assets/css/index.css?v=<?php echo time(); ?>">
     <!-- <link href="Assets/css/index.css" rel="stylesheet"> -->
     <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0..1,0" />
     <link
         href="https://fonts.googleapis.com/css2?family=Fira+Sans:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
         rel="stylesheet">
@@ -37,7 +41,178 @@
             </div>
         </div>
     </section>
+  <!-- 
+        - #PROPERTY SECTION
+      -->
 
+      <section class="section property" aria-labelledby="property-label">
+        <div class="container">
+
+          <div class="title-wrapper">
+
+            <div>
+              <h2 class="section-title headline-small">Best home in your city</h2>
+
+              
+            </div>
+
+           
+
+          </div>
+
+          <div class="property-list">
+
+            <div class="card">
+
+              <div class="card-banner">
+
+                <figure class="img-holder" style="--width: 585; --height: 390;">
+                  <img src="Assets/images/images/property-1.jpg" width="585" height="390" alt="COVA Home Realty" class="img-cover">
+                </figure>
+
+                <span class="badge label-medium">New</span>
+
+                <button class="icon-btn fav-btn" aria-label="add to favorite" data-toggle-btn>
+                  <span class="material-symbols-rounded" aria-hidden="true">favorite</span>
+                </button>
+
+              </div>
+
+              <div class="card-content" id="cardContent">
+
+                <span class="title-large">$710.68</span>
+
+                <h3>
+                  <a href="#" class="title-small card-title">COVA Home Realty</a>
+                </h3>
+
+                <address class="body-medium card-text">
+                  1901 Thornridge Cir. Shiloh, Hawaii 81063
+                </address>
+
+                <div class="card-meta-list">
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">bed</span>
+
+                    <span class="meta-text label-medium">3 Bed</span>
+                  </div>
+
+                
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">straighten</span>
+
+                    <span class="meta-text label-medium">1430 sqft </span>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="card">
+
+              <div class="card-banner">
+
+                <figure class="img-holder" style="--width: 585; --height: 390;">
+                  <img src="Assets/images/images/property-2.jpg" width="585" height="390" alt="Exit Realty" class="img-cover">
+                </figure>
+
+                <button class="icon-btn fav-btn" aria-label="add to favorite" data-toggle-btn>
+                  <span class="material-symbols-rounded" aria-hidden="true">favorite</span>
+                </button>
+
+              </div>
+
+              <div class="card-content">
+
+                <span class="title-large">$630.44</span>
+
+                <h3>
+                  <a href="#" class="title-small card-title">Exit Realty</a>
+                </h3>
+
+                <address class="body-medium card-text">
+                  2972 Westheimer Rd. Santa Ana, Illinois 85486
+                </address>
+
+                <div class="card-meta-list">
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">bed</span>
+
+                    <span class="meta-text label-medium">5 Bed</span>
+                  </div>
+
+                  
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">straighten</span>
+
+                    <span class="meta-text label-medium">1630 sqft </span>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+            </div>
+
+            <div class="card">
+
+              <div class="card-banner">
+
+                <figure class="img-holder" style="--width: 585; --height: 390;">
+                  <img src="Assets/images/images/property-3.jpg" width="585" height="390" alt="The Real Estate Group" class="img-cover">
+                </figure>
+
+                <button class="icon-btn fav-btn" aria-label="add to favorite" data-toggle-btn>
+                  <span class="material-symbols-rounded" aria-hidden="true">favorite</span>
+                </button>
+
+              </div>
+
+              <div class="card-content">
+
+                <span class="title-large">$475.22</span>
+
+                <h3>
+                  <a href="#" class="title-small card-title">The Real Estate Group</a>
+                </h3>
+
+                <address class="body-medium card-text">
+                  2118 Thornridge Cir. Syracuse, Connecticut 35624
+                </address>
+
+                <div class="card-meta-list">
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">bed</span>
+
+                    <span class="meta-text label-medium">8 Bed</span>
+                  </div>
+
+                 
+
+                  <div class="meta-item">
+                    <span class="material-symbols-rounded meta-icon" aria-hidden="true">straighten</span>
+
+                    <span class="meta-text label-medium">1240 sqft </span>
+                  </div>
+                  
+                </div>
+
+              </div>
+
+            </div>
+
+
+          </div>
+
+        </div>
+      </section>
 
 
     <?php
@@ -51,11 +226,14 @@
     include 'pages/contact/contactUs.php';
     ?>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
         crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+<script src="https://unpkg.com/ionicons@4.5.10-0/dist/ionicons.js"></script> -->
+<script src="Assets/js/index.js"></script>
 </body>
 
 </html>
---
+++ pages/property/property_edit.php
@@ -0,0 +1,138 @@
+<?php
+            include '../../Components/header.php';
+            $property_id=(isset($_GET["id"]))?$_GET["id"]:null;
+            if($property_id){
+                $sql_query=$db_connection->prepare("SELECT * FROM property WHERE property_id=?");
+                if($sql_query){
+                    // Bind parameters to the prepared statement
+                    $sql_query->bind_param("s",$property_id);
+                    $sql_query->execute();
+
+                    // Get result
+                    $result = $sql_query->get_result();
+
+                    // Check if any rows were returned
+                    if ($result->num_rows > 0) {
+                        // Fetch data
+                        while ($row = $result->fetch_assoc()){
+                            $selected_property=[];
+                            foreach ($row as $key => $value)
+                                $selected_property[$key]=$value;
+                                // echo $key." - ".$value."<br>";
+                        }
+                    }
+                    else $_SESSION["Response"]="Property does not exist";
+                }
+                else $_SESSION["Errors"]="System Error: Contact Support";
+            }
+            else $_SESSION["Errors"]="Error: Missing Property ID"
+        ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Property Edit Form</title>
+    <link rel="stylesheet" href="../../Assets/css/index.css?v=<?php echo time(); ?>">
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <style>
+        body {
+            font-family: 'Fira Sans', sans-serif;
+        }
+        .form-group {
+            margin-bottom: 20px;
+        }
+        label {
+            font-weight: 600;
+        }
+    </style>
+</head>
+
+<body>
+
+    <section class="property_edit">
+        <div class="container">
+            <div class="title-wrapper">
+                <h2 class="section-title headline-small">Edit Property</h2>
+            </div>
+            <form action="../../control/property_edit.php" method="POST">
+                <div class="row">
+                    <div class="col-md-3">
+                        <div class="form-group">
+                            <label for="name">Name</label>
+                            <input type="text" name="name" class="form-control" id="name" placeholder="Name" 
+                            value="<?php echo (isset($selected_property['Name']))?$selected_property['Name']:'';?>">
+
+                        </div>
+                        <div class="form-group">
+                            <label for="address">Address</label>
+                            <input type="text" name="address" class="form-control" id="address" placeholder="Address"
+                            value="<?php echo (isset($selected_property['address']))?$selected_property['address']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="eircode">Eircode</label>
+                            <input type="text" name="eircode" class="form-control" id="eircode" placeholder="Eircode"
+                            value="<?php echo (isset($selected_property['eircode']))?$selected_property['eircode']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="category">Category</label>
+                            <select class="custom-select">
+                            <option selected disabled>Choose category</option>
+                                <option value="house" 
+                                <?php if(isset($selected_property['category']) && $selected_property['category']=='house') echo "selected";?>
+                                >House</option>
+                                <option value="apartment"
+                                <?php if(isset($selected_property['category']) && $selected_property['category']=='apartment') echo "selected";?>
+                                >Apartment</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label for="price">Price</label>
+                            <input type="number" name="price" class="form-control" id="price" placeholder="Price"
+                            value="<?php echo (isset($selected_property['price']))?$selected_property['price']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="start_date">Start Date</label>
+                            <input type="date" name="start_date" class="form-control" id="start_date"
+                            value="<?php echo (isset($selected_property['start_date']))?$selected_property['start_date']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="end_date">End Date</label>
+                            <input type="date" name="end_date" class="form-control" id="end_date"
+                            value="<?php echo (isset($selected_property['end_date']))?$selected_property['end_date']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="description">Description</label>
+                            <textarea type="text" name="description" class="form-control" id="description"
+                                placeholder="Description" >
+                                <?php echo (isset($selected_property['description']))?$selected_property['description']:'';?></textarea>
+                        </div>
+                        <div class="form-group">
+                            <label for="num_beds">Number of Beds</label>
+                            <input type="number" name="num_beds" class="form-control" id="num_beds"
+                                placeholder="Number of Beds"  value="<?php echo (isset($selected_property['num_beds']))?$selected_property['num_beds']:'';?>">
+                        </div>
+                        <div class="form-group">
+                            <label for="size">Size</label>
+                            <input type="text" name="size" class="form-control" id="size" placeholder="Size"
+                             value="<?php echo (isset($selected_property['size']))?$selected_property['size']:'';?>">
+                        </div>
+                        <button id="btn-editP"type="submit" class="btn btn-outline-primary" data-mdb-ripple-init>Update property</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
